67eff5a6dd8717c5ae145f06d7750d8b3a61e515
Lakshay Madhav <lakshaymadhav25@gmail.com>
2021-06-11 21:30:30 +0530

    initial commit

---
+++ encrypt2.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+void compress(string s)
+{
+    
+    string ans;
+    int n=1;
+    int si=s.size();
+    for(int i=0;i<si;i++)
+    {
+        if(s[i]!=s[i+1])
+        {
+            ans+=s[i];
+            ans+=to_string(n);
+            n=1;
+        }
+        else
+        {
+            n++;
+        }
+    }
+    cout<<"compressed: "+ans;
+}
+void decompress(string s)
+{
+    
+    int si=s.size();
+    string ans;
+    string a1;
+    for(int i=0;i<si;i++)
+    {
+        if(s[i]>='a' && s[i]<='z' || s[i]>='A' && s[i]<='Z')
+        {
+            a1 =s[i];
+        }
+        else
+        {
+            int m=int(s[i])-48;
+            
+            for(int j=0;j<m;j++)
+            {
+                ans+=a1;
+            }
+            a1="";
+        }
+    }
+    cout<<ans;
+}
+int main(){
+    string s;
+    cin>>s;
+    int ch,chn;
+    cout<<"1.compress\n";
+    cout<<"2.decompress\n";
+    cin>>chn;
+    do
+    {
+        switch (ch)
+    {
+    case 1:
+        compress(s);
+        break;
+    case 2:
+    decompress(s);
+    break;
+    }
+    } while (chn==1);
+    
+    
+}
